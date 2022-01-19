d97546762af246d6bf694093b414af5d2c3107b2
Gadgistan4k <90455483+Gadgistan4k@users.noreply.github.com>
2022-01-19 11:33:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,55 @@
 f = open('pokemon_full.json')
-file = f.read()
+pokemons = f.read()
 
-vse = len(file)  # Ищем количество символов в файле
+all = len(pokemons)  # Ищем количество символов в файле
 
-not_probel = file.replace(" ", "")  # Убираем все пробелы из файла
-znaki = 0
+not_space = pokemons.replace(" ", "")  # Убираем все пробелы из файла
+signs = 0
 for i in ('.', ',', '[', ']', '(', ')', ':', ';', '{', '}', "'", '"'):
-    znaki = znaki + file.count(i)  # Считаем количество всех знаков препинания
-vse2 = len(not_probel) - znaki  # Получаем искомый ответ
+    signs = signs + pokemons.count(i)  # Считаем количество всех знаков препинания
+all2 = len(not_space) - signs  # Получаем искомый ответ
 
 f2 = open('pokemon_full.json')
-file2 = f2.readlines()
-max_opisanie = 0
-for i in range(0, len(file2)):
-    stroka = file2[i]
-    if stroka.count('"name"') == 1:
+pokemons2 = f2.readlines()
+max_description = 0
+for i in range(0, len(pokemons2)):
+    line = pokemons2[i]
+    if line.count('"name"') == 1:
         # Находим имя покемона для которго дано писание
-        name = stroka
-    if stroka.count('"description"') == 1:
-        opisanie = len(stroka)
-        if opisanie > max_opisanie:
+        name = line
+    if line.count('"description"') == 1:
+        description = len(line)
+        if description > max_description:
             # Находим максимальную длину описания
-            max_opisanie = opisanie
-            name_with_max_opisanie = name
-name_with_max_opisanie = ((name_with_max_opisanie.replace('"name": "', '')).replace('",', '')).replace('    ', '')
+            max_description = description
+            name_with_max_description = name
+name_with_max_description = ((name_with_max_description.replace('"name": "', '')).replace('",', '')).replace('    ', '')
 # Избавимся от лишних символов в строке кроме имени
 
-slov = 0
-for i in range(0, len(file2)):
-    s = 0
-    n = 1
-    stroka = file2[i]
-    if stroka.count('"abilities"') == 1:
+words = 0
+for i in range(0, len(pokemons2)):
+    count = 0
+    count_2 = 1
+    line = pokemons2[i]
+    if line.count('"abilities"') == 1:
         # Находим начало описания способностей и запускаем цикл по перебору их
-        while s != 1:
-            stroka = file2[i + n]
-            n = n + 1
-            if stroka.count(']') == 1:
+        while count != 1:
+            line = pokemons2[i + count_2]
+            count_2 = count_2 + 1
+            if line.count(']') == 1:
                 # Заверщаем цикл перебора
-                s = 1
-            if stroka.count(" ") - stroka.count(' "') > slov:
+                count = 1
+            if line.count(" ") - line.count(' "') > words:
                 # По количеству пробелов определяем максимальную длину слова
-                # И исключаем ощибку где способность написанна с пробелом в конце
-                slov = stroka.count(" ")
-                abilit = stroka
+                # И исключаем ошибку где способность написанна с пробелом в конце
+                words = line.count(" ")
+                abilit = line
 abilit = (abilit.replace('"', '')).replace('      ', '')
 # Очищаем название способности от лишних символов
 
-print('Общее количество символов в файле:', vse)
+print(f'Общее количество символов в файле:{all}')
 print('')
-print('общее количесто символов без пробелов и знаков препинания:', vse2)
+print(f'Общее количесто символов без пробелов и знаков препинания:{all2}')
 print('')
-print('Покемон с самым длинным описанием:', name_with_max_opisanie)
-print('Способность с наибольшим количеством слов:', abilit)
+print(f'Покемон с самым длинным описанием:{name_with_max_description}')
+print(f'Способность с наибольшим количеством слов:{abilit}')
